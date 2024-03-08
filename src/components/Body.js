36837254976f48ecabb3390b688fe6a112c529fa
21tushar <prasant324@gmail.com>
2024-03-08 11:07:34 +0530

    Search Functionality Added

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,11 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listofRestaurants, setlistOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  function getSearchText(e) {
+    setSearchText(e.target.value);
+  }
 
   useEffect(() => {
     fetchData();
@@ -12,7 +17,7 @@ const Body = () => {
 
   async function fetchData() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
     );
 
     const res = await data.json();
@@ -26,10 +31,30 @@ const Body = () => {
   return listofRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
-    <div className="body">
-      <div className="p-[10px] ml-[35px] mb-3">
+    <div>
+      <div className="flex gap-1 mt-4">
+        <input
+          type="text"
+          className="bg-[#f4f4f4] rounded-xl ml-[2.78rem] pl-2"
+          onChange={getSearchText}
+          value={searchText}
+        />
+
         <Button
-          className="rounded-3xl iPhone4and4S:ml-6 iPhone6+and7+and8+:ml-12 iPad1and2andMiniandAir:ml-9 FullHDand2K:ml-56 font-semibold"
+          className="rounded-xl"
+          onClick={() => {
+            const filteredRestaurants = listofRestaurants.filter((res) => {
+              res.info.name.includes(searchText);
+            });
+
+            setlistOfRestaurants(filteredRestaurants);
+          }}
+        >
+          Search
+        </Button>
+
+        <Button
+          className="rounded-xl iPhone4and4S:ml-6 iPhone6+and7+and8+:ml-12 iPad1and2andMiniandAir:ml-9 FullHDand2K:ml-56 font-semibold p-[10px] ml-5"
           onClick={() => {
             const filterRestaurants = listofRestaurants.filter(
               (res) => res.info.avgRating > 4.3
@@ -40,7 +65,7 @@ const Body = () => {
           Top Rated Restaurants
         </Button>
       </div>
-      <div className="res-container gap-2 grid grid-cols-4 iPhone4and4S:grid iPhone4and4S:grid-cols-1 iPhone4and4S:ml-5 iPad1and2andMiniandAir:grid iPad1and2andMiniandAir:grid-cols-2 GalaxyTab2:grid GalaxyTab2:grid-cols-3  GalaxyTab2:ml-10 iPhone6+and7+and8+:ml-12 iPad1and2andMiniandAir:ml-[2.3rem] iPadPro10.5:mr-[4.5rem] FullHDand2K:ml-56">
+      <div className="res-container mt-6 gap-2 grid grid-cols-4 iPhone4and4S:grid iPhone4and4S:grid-cols-1 iPhone4and4S:ml-5 iPad1and2andMiniandAir:grid iPad1and2andMiniandAir:grid-cols-2 GalaxyTab2:grid GalaxyTab2:grid-cols-3  GalaxyTab2:ml-10 iPhone6+and7+and8+:ml-12 iPad1and2andMiniandAir:ml-[2.3rem] iPadPro10.5:mr-[4.5rem] FullHDand2K:ml-56">
         {listofRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
