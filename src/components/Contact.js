5ddac64e4ead0b7143c970bb00c1de1868e4d879
21tushar <prasant324@gmail.com>
2024-03-26 19:27:11 +0530

    media queries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,24 +14,25 @@ const Contact = () => {
   return (
     <>
       <Header />
-      <div className="grid grid-cols-2 mt-24">
+      <div className="grid grid-cols-2 mt-24 iPhone4and4S:grid iPhone4and4S:grid-cols-1 iPhone4and4S:mt-11">
         <div className="ml-32">
           <img
             src="https://foodfire-chapter09.netlify.app/Contact-Us.13c5d28a.png"
             alt=""
             width={350}
+            className="iPhone4and4S:-ml-16"
           />
         </div>
-        <div className="-ml-40">
+        <div className="-ml-40 iPhone4and4S:justify-center iPhone4and4S:items-center iPhone4and4S:text-center iPhone4and4S:ml-4 iPhone4and4S:mt-7 iPhone4and4S:*:mr-5">
+        <h1 className="text-[2.3em] iPhone4and4S:text-[3em] font-bold font-poppins text-center mt-3">Contact us</h1>
           <form
-            className="flex-col items-center justify-center flex"
+            className="flex-col items-center justify-center flex iPhone4and4S:*:w-60"
             onSubmit={handleSubmit}
           >
-            <h1 className="text-[2.3em] font-bold font-poppins">Contact us</h1>
             <Input
               isRequired
               type="text"
-              className="pl-2 w-[38rem] mt-1"
+              className="pl-2 w-[38rem] mt-2"
               placeholder="Enter your name"
               size="sm"
               radius="sm"
